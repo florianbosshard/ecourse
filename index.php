9b388628a843b3d678bcc3bcefc10a36121b49a1
Florian Bosshard <florian.bosshard@gmail.com>
2015-10-14 17:23:38 +0200

    added menu on top of page

--- index.php
+++ index.php
@@ -16,7 +16,35 @@ if(!isset($_SESSION["leaderID"]))
   <link rel="stylesheet" href="css/app.css">
   </head>
 <body>
-<strong><font color="red">Bei längerer Pause (z.B. Nacht) bitte <a href="logout.php">ausloggen</a> und neu einloggen. Sonst kann es sein, dass eure Beobachtungen verloren gehen! Sorry!</font></strong>
+    
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#/">Ecourse</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="#/">TNFG</a></li>
+        <li><a href="#/activities/">Aktivitäten</a></li>
+        <li><a href="#/participants/">Teilnehmende</a></li>
+        
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+    
     <div class="container">
         <div ng-view></div>
     </div>

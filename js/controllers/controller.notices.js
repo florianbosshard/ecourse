cfe5fa89e02de307ee93b550889efd446f77b1fe
Florian Bosshard <florian.bosshard@gmail.com>
2016-03-13 22:00:23 +0100

    refactoring

--- js/controllers/controller.notices.js
+++ js/controllers/controller.notices.js
@@ -1,24 +1,5 @@
 
-app.controller('BeobachtungController', ['$http', '$scope', 'BeobachtungFactory', '$routeParams', '$location', function($http, $scope, BeobachtungFactory, $routeParams, $location){
-    $scope.beobachtung = null;
-    var participantId = $routeParams.id;
-
-
-    $http.get('api/index.php/days/')
-   .success(
-       function(data, status, headers, config) {
-
-           $scope.days = data;
-       })
-   .error(
-       function(data, status, headers, config) {
-
-           $scope.days = status;
-       });
-
-
-
-
+function getCategories($http, $scope){
      $http.get('api/index.php/categories/')
         .success(
             function(data, status, headers, config) {
@@ -30,86 +11,35 @@ app.controller('BeobachtungController', ['$http', '$scope', 'BeobachtungFactory'
 
                 $scope.categories = status;
             });
-    $scope.timeDisabled = true;
+}
+function getDays($http, $scope){
+      $http.get('api/index.php/days/')
+   .success(
+       function(data, status, headers, config) {
 
-    $scope.$watch('beobachtung.date', function(newVal) {
-        if (newVal){
-            $scope.timeDisabled = false;
-            $http.get('api/index.php/activityPerDate/'+ newVal)
-                .success(
-                    function(data, status, headers, config) {
+           $scope.days = data;
+       })
+   .error(
+       function(data, status, headers, config) {
 
-                    $scope.dayActivities = data;
-                })
-                .error(
-                    function(data, status, headers, config) {
+           $scope.days = status;
+       });
+}
 
-                    $scope.dayActivities = status;
-                });
-            }
-    });
-    $scope.$watch('beobachtung.activityId', function(newVal) {
-           if (!$scope.dayActivities || (newVal && newVal != 0)){
-               $scope.timeDisabled = true;
-           } else {
-               $scope.timeDisabled = false;
-           }
-    });
 
-    var formatTime = function(time) {
-        var result = false, m;
-        var re = /^\s*([01]?\d|2[0-3]):?([0-5]\d)\s*$/;
-        if ((m = time.match(re))) {
-            result = (m[1].length == 2 ? "" : "0") + m[1] + ":" + m[2];
-        }
-        return result;
+function formatTime(time) {
+    var result = false, m;
+    var re = /^\s*([01]?\d|2[0-3]):?([0-5]\d)\s*$/;
+    if ((m = time.match(re))) {
+        result = (m[1].length == 2 ? "" : "0") + m[1] + ":" + m[2];
     }
+    return result;
+}
 
-    $scope.addBeobachtung = function () {
-        if(!$scope.beobachtung){
-            alert('Wie w&auml;re es, wenn du noch etwas in die Felder eingeben w&uuml;rdest? ;-)')
-        } else if((!$scope.beobachtung.activityId || $scope.beobachtung.activityId == 0) && !$scope.beobachtung.time){
-            alert('Entweder muss eine Aktivit&auml;t ausgew&auml;hlt oder eine Zeit eingegeben werden.')
-        } else if($scope.beobachtung.activityId > 0 && $scope.beobachtung.time){
-            alert('Es soll entweder eine Aktivit&auml;t oder ein Zeitpunkt ausgew&auml;hlt werden.')
-        } else if($scope.beobachtung.time && !formatTime($scope.beobachtung.time)){
-            alert('Die Zeit muss in der Form HH:MM eingegeben werden!');
-        }else if(!$scope.beobachtung.categoryId){
-            alert('Die Kategorie fehlt.');
-        } else if(!$scope.beobachtung.beobachtung){
-            alert('Hast du nicht noch etwas vergessen? Das Feld mit der Beobachtung ist leer. ;-)')
-        }
-        else{
-
-
-            $scope.beobachtung.participantId = participantId;
-            BeobachtungFactory.create($scope.beobachtung)
-            $location.path('/participants/view/'+ participantId);
-        }
-    }
-
-}]);
-
-
-app.controller('BeobachtungModifyController', ['$http', '$scope', 'BeobachtungFactory', '$routeParams', '$location', function($http, $scope, BeobachtungFactory, $routeParams, $location){
-    $scope.beobachtung = null;
-    var beobachtungId = $routeParams.id;
+function getCurrentBeobachtung($http, $scope, beobachtungId){   
+    console.log('get current beobachtung');
     
-    $http.get('api/index.php/days/')
-            .success(
-                function(data, status, headers, config) {
-
-                    $scope.days = data;
-                    getCurrentBeobachtung();
-                })
-            .error(
-                function(data, status, headers, config) {
-
-                    $scope.days = status;
-                });
-    
-    function getCurrentBeobachtung(){   
-        $http.get('api/index.php/beobachtung/'+beobachtungId)
+    $http.get('api/index.php/beobachtung/'+beobachtungId)
     .success(
         function(data, status, headers, config) {
             console.log(data);   
@@ -132,25 +62,10 @@ app.controller('BeobachtungModifyController', ['$http', '$scope', 'BeobachtungFa
             $scope.beobachtung = status;
         });
     
-    }
+}
 
-
-
-
-     $http.get('api/index.php/categories/')
-        .success(
-            function(data, status, headers, config) {
-
-                $scope.categories = data;
-            })
-        .error(
-            function(data, status, headers, config) {
-
-                $scope.categories = status;
-            });
-    $scope.timeDisabled = true;
-
-    $scope.$watch('beobachtung.date', function(newVal) {
+function watchDateChange($scope, $http){
+        $scope.$watch('beobachtung.date', function(newVal) {
         if (newVal){
             $scope.timeDisabled = false;
             $http.get('api/index.php/activityPerDate/'+ newVal)
@@ -166,6 +81,8 @@ app.controller('BeobachtungModifyController', ['$http', '$scope', 'BeobachtungFa
                 });
             }
     });
+}
+function watchActivityIdChange($scope){
     $scope.$watch('beobachtung.activityId', function(newVal) {
            if (!$scope.dayActivities || (newVal && newVal != 0)){
                $scope.timeDisabled = true;
@@ -173,19 +90,19 @@ app.controller('BeobachtungModifyController', ['$http', '$scope', 'BeobachtungFa
                $scope.timeDisabled = false;
            }
     });
+}
 
-    var formatTime = function(time) {
-        var result = false, m;
-        var re = /^\s*([01]?\d|2[0-3]):?([0-5]\d)\s*$/;
-        if ((m = time.match(re))) {
-            result = (m[1].length == 2 ? "" : "0") + m[1] + ":" + m[2];
-        }
-        return result;
-    }
+function initFormValues($scope, $http){
+    getCategories($http, $scope);
+    getDays($http, $scope);
+    
+    $scope.timeDisabled = true;
+    watchDateChange($scope, $http);
+    watchActivityIdChange($scope);
+    console.log('finished');
+}
 
-    $scope.modifyBeobachtung = function () {
-        
-        console.log('hallo modify');
+function validateBeobachtung($scope){
         if(!$scope.beobachtung){
             alert('Wie w&auml;re es, wenn du noch etwas in die Felder eingeben w&uuml;rdest? ;-)')
         } else if((!$scope.beobachtung.activityId || $scope.beobachtung.activityId == 0) && !$scope.beobachtung.time){
@@ -198,10 +115,39 @@ app.controller('BeobachtungModifyController', ['$http', '$scope', 'BeobachtungFa
             alert('Die Kategorie fehlt.');
         } else if(!$scope.beobachtung.beobachtung){
             alert('Hast du nicht noch etwas vergessen? Das Feld mit der Beobachtung ist leer. ;-)')
+        } else {
+            return true;
         }
-        else{
+}
+
+app.controller('BeobachtungController', ['$http', '$scope', 'BeobachtungFactory', '$routeParams', '$location', function($http, $scope, BeobachtungFactory, $routeParams, $location){
+    $scope.beobachtung = null;
+    var participantId = $routeParams.id;
+    
+    initFormValues($scope, $http);
 
 
+    $scope.addBeobachtung = function () {
+        if(validateBeobachtung($scope)===true){
+            $scope.beobachtung.participantId = participantId;
+            BeobachtungFactory.create($scope.beobachtung)
+            $location.path('/participants/view/'+ participantId);
+        }
+    }
+
+}]);
+
+
+app.controller('BeobachtungModifyController', ['$http', '$scope', 'BeobachtungFactory', '$routeParams', '$location', function($http, $scope, BeobachtungFactory, $routeParams, $location){
+    $scope.beobachtung = null;
+    var beobachtungId = $routeParams.id;
+    initFormValues($scope, $http);
+    
+    getCurrentBeobachtung($http, $scope, beobachtungId)
+
+    $scope.modifyBeobachtung = function () {
+        
+        if(validateBeobachtung($scope)===true){
             BeobachtungFactory.update($scope.beobachtung)
             $location.path('/participants/view/'+ $scope.beobachtung.participantId);
         }
